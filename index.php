cf8ac65a78c3e350c8d44a7f824d6ef554082693
SellyUtari <sellyutari76@gmail.com>
2024-10-10 14:44:53 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "Selly";
+$umur = 19;
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contoh Variabel php</title>
+</head>
+<body>
+    <h1>Halo,<?php echo $nama; ?>!</h1>
+    <p>Umur Anda adalah <?php echo $umur; ?> tahun.</p>
+</body>
+</html>
